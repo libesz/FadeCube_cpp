9f7a728240c712171b32cf0525b45d02c94b9ca0
libesz <huszty.gergo@digitaltrip.hu>
2014-04-03 00:37:30 +0200

    dummy snake app added

--- snake.cpp
+++ snake.cpp
@@ -6,8 +6,24 @@
  */
 
 #include <Snake.h>
+#include <CubeDisplay.h>
+#include <Renderer.h>
+#include <vector>
 
-int main(void) {
+using namespace FadeCube;
 
+int main( int argc, char **argv ) {
+  Snake s(10,10,10);
+  std::vector<Renderable *> ra;
+  ra.push_back(&s);
+
+  CubeDisplay d("192.168.1.99", 1125);
+  Renderer re(&d);
+  s.start(0,0,0, Snake::Direction::FORWARD);
+  s.move();
+  s.move();
+  s.move();
+  s.move();
+  re.draw(ra);
 }
 
